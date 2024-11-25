b4bb5ae99d86ba5ba90a89c9c4e7ea53b65b4583
diegohuertan <124300301+diegohuertan@users.noreply.github.com>
2024-11-25 20:50:46 -0300

    funcionalidad leer proceso actual

--- certamen.go
+++ certamen.go
@@ -23,8 +23,44 @@ func (d *dispatcher) agregarProceso(pid int, estado string, contadorProg int, no
 	d.colaprocesos = append(d.colaprocesos, bcp{pid, estado, contadorProg, nombreproceso})
 }
 
-func (d *dispatcher) ejecutarDispatcher(procesoActual chan bcp, tiempoEjecucion int) {
+func (d *dispatcher) ejecutarProceso(procesoActual chan bcp, tiempoEjecucion int) {
+	for proceso := range procesoActual {
+		nombreProceso := proceso.nombreproceso
 
+		file, err := os.Open(nombreProceso)
+		if err != nil {
+			fmt.Println("Error al abrir el archivo:", err)
+			return
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		linea := 0
+		lineasEjecutadas := 0
+		encontrado := false
+		for scanner.Scan() {
+			linea++
+			if linea == proceso.contadorProg {
+				encontrado = true
+			}
+			if encontrado {
+				fmt.Println(scanner.Text())
+				lineasEjecutadas++
+				if lineasEjecutadas == tiempoEjecucion {
+					proceso.contadorProg = linea
+					fmt.Printf("Contador actualizado del proceso %d: %d\n", proceso.pid, proceso.contadorProg)
+					d.contadorDisp = linea
+					fmt.Printf("Contador del dispatcher actualizado: %d\n", d.contadorDisp)
+					procesoActual <- proceso
+					break
+				}
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error al leer el archivo:", err)
+		}
+	}
 }
 
 func leerArchivo(ruta string, lineas chan<- string, wg *sync.WaitGroup) {
@@ -51,16 +87,26 @@ func leerArchivo(ruta string, lineas chan<- string, wg *sync.WaitGroup) {
 }
 
 func main() {
-	lineas := make(chan string)
+	procesoActual := make(chan bcp)
 	var wg sync.WaitGroup
 
-	wg.Add(1)
-	go leerArchivo("Orden_creacion.txt", lineas, &wg)
+	d := &dispatcher{}
+
+	d.agregarProceso(1, "running", 1, "proceso_1")
+	d.agregarProceso(2, "running", 6, "proceso_2")
 
 	go func() {
-		for linea := range lineas {
-			fmt.Println(linea)
+		for _, proceso := range d.colaprocesos {
+			procesoActual <- proceso
 		}
+		close(procesoActual)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		d.ejecutarProceso(procesoActual, 5)
+
 	}()
 
 	wg.Wait()
